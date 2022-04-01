a5fc9d67f765992e6e738bb7c5f6f5b9bc203c19
Shami <shamitomita@gmail.com>
2022-04-01 18:01:47 -0400

    hooks with adding list items

--- my-practice/src/App.js
+++ my-practice/src/App.js
@@ -4,6 +4,22 @@ function App() {
   const [count, setCount] = useState(0)
   const [theme, setTheme] = useState("blue")
   const [style, setStyle] = useState("cont")
+  const [items, setItems] = useState([])
+  const [userInput, setUserInput] = useState('')
+
+
+const addItem = item => {
+  console.log(item)
+  const newItems = [...items, item];
+  setItems(newItems);
+  console.log(items)
+}
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    addItem(userInput);
+    setUserInput("")
+  }
 
   function changeStyle(cont){
     setStyle(cont)
@@ -21,12 +37,38 @@ function App() {
   }
   return (
     <div className="App">
-      <button onClick={decrement}>-</button>
-      <span>{count}</span>
-      <span className={style}>{theme}</span>
-      <button onClick={increment}>+</button>
-    </div>
-  );
-}
+      <div className="counter">
+        <button onClick={decrement}>-</button>
+        <span>{count}</span>
+        <span className={style}>{theme}</span>
+        <button onClick={increment}>+</button>
+      </div>
+      <br/>
+      <div className="list">
+
+        <form onSubmit={handleSubmit}>
+          <label>Enter Your Item
+            <input
+              type="text"
+              value={userInput}
+              onChange={(e) => setUserInput(e.target.value)}
+            />
+          </label>
+          <input type="submit"/>
+          <br></br>
+          {userInput}
+        </form>
+        <div>
+          <h4>Your Items:</h4>
+          {items.map((item, index) => (
+            <div key={index}>
+              <p>{item}</p>
+            </div>
+          ))}
+        </div>
+        </div>
+      </div>
+      );
+      }
 
 export default App;
